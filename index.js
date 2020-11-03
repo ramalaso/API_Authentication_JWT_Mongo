863c8164589b9afbf05daa92feff193a012797c0
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-11-03 17:04:09 -0400

    Added post method

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 //Import routes
 const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true, useNewUrlParser: true }, () => {
+    console.log('Connected to DB.');
+});
+
+
+//Middleware
+app.use(express.json());
 
 //Routes middlewares
 app.use('/api/user', authRoute);
@@ -10,3 +22,4 @@ app.use('/api/user', authRoute);
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
+
