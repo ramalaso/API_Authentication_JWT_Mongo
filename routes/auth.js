464b6eb447c79d186697d4ed4a725978f38df447
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-11-03 19:17:34 -0400

    Added validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
+//Validation
+const Joi = require('joi');
+const schema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+});
+
 router.post('/register', async (req, res) => {
+
+    //Lets validate the data before we created a user
+    const { error } = schema.validate(req.body);
+    if (error) return res.status(400).send(error);
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
@@ -9,7 +22,7 @@ router.post('/register', async (req, res) => {
     });
     try {
         const savedUser = await user.save();
-        res.send({ "user": savedUser });
+        res.send(savedUser);
     } catch (error) {
         res.status(400).send(error);
     }
