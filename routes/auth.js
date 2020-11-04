91111c9c766efb48bacc002091f9d170b24b44d0
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-11-03 20:46:35 -0400

    Added sessions

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 //Validation
 const Joi = require('joi');
@@ -45,7 +46,9 @@ router.post('/login', async (req, res) => {
     const validPass = await bcrypt.compare(req.body.password, user.password);
     if (!validPass) return res.status(400).send('invalid Password');
 
-    res.send('Logged in');
+    //Create and assign a token
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
 });
 
 module.exports = router;
