3658a2a50614dee6e9dfe4daa88441544967741a
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-11-03 20:23:51 -0400

    Added authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 //Validation
 const Joi = require('joi');
@@ -15,10 +16,18 @@ router.post('/register', async (req, res) => {
     const { error } = schema.validate(req.body);
     if (error) return res.status(400).send(error);
 
+    //Checking if the user is already in the satabase
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send('Email already exists.');
+
+    //Hash passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     });
     try {
         const savedUser = await user.save();
@@ -28,6 +37,15 @@ router.post('/register', async (req, res) => {
     }
 });
 
-router.post('/login');
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send('Email or password is wrong.');
+
+    //Checking if password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).send('invalid Password');
+
+    res.send('Logged in');
+});
 
 module.exports = router;
